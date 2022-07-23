3e09cec1dc80573c20173b2806685f6e0279ef04
solidspoon <cedarrival@outlook.com>
2022-07-23 10:51:02 +0800

    feat: types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 // mod print;
-mod vars;
+// mod vars;
+mod types;
+
 
 // Make it work, don't modify `implicitly_ret_unit` !
 fn main() {
    // print::run();
-   vars::run();
+   // vars::run();
+   types::run();
 }
+
